956079234e90bd29bd40547924038c097a75e693
azemazer <a.laval16@laposte.net>
2023-04-27 09:58:46 +0200

    database fake data

--- todo/app/Http/Controllers/TodosController.php
+++ todo/app/Http/Controllers/TodosController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use SebastianBergmann\Template\Template;
+use App\Models\Todos;
 
 class TodosController extends Controller
 {
     public function liste(){
-        return view("template");
+        return view("home", ["todos" => Todos::all()]);
     }
     // public function __invoke(){
     //     //...
